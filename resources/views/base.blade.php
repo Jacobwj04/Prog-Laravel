276b77c73336942b92e6503444d92d422bedc83b
Jacob Wassall-jamieson <jacob@thewj5.com>
2023-12-05 10:44:53 +0100

    added controllers

---
+++ resources/views/base.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/dist/app.css">
+    <title>Document</title>
+</head>
+<body class="">
+    <header class="flex gap-4">
+        <a href="">
+            Logo
+            <img src="" alt="">
+        </a>
+        <ul class="flex gap-4">
+            <li class="text-4xl">Home</li>
+            <li class="text-4xl">About</li>
+            <li class="text-4xl">
+                <a href="/projecten">Projecten</a>
+            </li>
+        </ul>
+    </header>
+    <section class="mt-10 flex flex-col gap-4">
+        <h1 class="text-4xl">About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt corrupti aperiam asperiores molestiae voluptates assumenda impedit illum. Aperiam doloremque possimus vitae qui, in quas voluptatibus natus, amet rem repellendus ab?</p>
+    </section>
+</body>
+</html>
---
+++ resources/views/project/index.blade.php
@@ -0,0 +1,5 @@
+<section>
+    <h2 class="font-2xl">Welkom op mijn site</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempore ipsa perspiciatis odit quia error, pariatur assumenda explicabo unde voluptas aperiam nisi sequi cupiditate! Repellat eum possimus consequuntur quia sequi.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo tempore ipsa perspiciatis odit quia error, pariatur assumenda explicabo unde voluptas aperiam nisi sequi cupiditate! Repellat eum possimus consequuntur quia sequi.</p>
+</section>
